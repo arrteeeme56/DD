7a977c8685db8f34dd0437ad8d6e1b67f5dc6e75
Sergey Royz <zjor.se@gmail.com>
2023-06-19 15:36:20 +0200

    fixed & removed target

--- WhiteBananaBot/src/main/java/io/proj3ct/WhiteBananaBot/config/BotInitializer.java
+++ WhiteBananaBot/src/main/java/io/proj3ct/WhiteBananaBot/config/BotInitializer.java
@@ -7,8 +7,6 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
-import org.telegram.telegrambots.meta.generics.BotSession;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Component
@@ -19,15 +17,7 @@ public class BotInitializer {
 
     @EventListener({ContextRefreshedEvent.class})
     public void init() throws TelegramApiException {
-
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-        try {
-            telegramBotsApi.registerBot(bot);
-        }
-
-        catch (TelegramApiException e) {
-
-        }
-
+        telegramBotsApi.registerBot(bot);
     }
 }
